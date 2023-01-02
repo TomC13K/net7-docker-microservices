8f406ffc99cdfeb238f3c9162ff7f44e1ea23fb9
ForkTest <testFork@email.com>
2023-01-02 00:54:24 +0000

    fixed query in createDiscount method

--- Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -31,8 +31,8 @@ public class DiscountRepository : IDiscountRepository
         using var connection =
             new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
         var affected = await connection.ExecuteAsync(
-            "INSERT INTO Coupon (ProductName, Description,Amount) VALUES (@ProductName, @Description, @Amount," +
-            "new(ProductName = coupon.ProductName, Description - coupon.Description, Amount = coupon.Amount))");
+            "INSERT INTO Coupon (ProductName, Description,Amount) VALUES (@ProductName, @Description, @Amount)",
+            new {ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount});
 
         if (affected == 0)
             return false;
